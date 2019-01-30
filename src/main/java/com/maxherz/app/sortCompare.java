ecbd41ddf9b3cd935e464433c49e6ddd85f39ae7
Max Herz <maxopie@gmail.com>
2019-01-30 18:35:17 -0500

    Optimize selection for time and space

--- src/main/java/com/maxherz/app/sortCompare.java
+++ src/main/java/com/maxherz/app/sortCompare.java
@@ -4,6 +4,7 @@ import org.apache.poi.openxml4j.exceptions.*;
 import org.apache.poi.ss.usermodel.*;
 import java.io.File;
 import java.io.IOException;
+import java.security.Policy;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -13,49 +14,41 @@ import java.util.Iterator;
  *
  */
 public class sortCompare {
-    public static final String SAMPLE_XLSX_FILE_PATH = "./testData.xlsx";
 
     public static void main(String[] args) throws IOException, InvalidFormatException {
+        String SAMPLE_XLSX_FILE_PATH = "./testData.xlsx"; //path of excel file
         Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH)); // get workbook
         Sheet sheet = workbook.getSheetAt(0); // first sheet
-        DataFormatter dataFormatter = new DataFormatter();
-        ArrayList<String>all = new ArrayList<>();
         Row row = sheet.getRow(0); //row
+        final int rowSize = 6; //rowsize. saves space
         int numSelected = 2; //how many random numbers to sample
 
-        Iterator<Cell> cellIterator = row.cellIterator();
-        int i=0;
-        while (cellIterator.hasNext()) { //populate arraylist with all row numbers
-            Cell cell = cellIterator.next(); 
-            String val = dataFormatter.formatCellValue(cell);
-            all.add(i, val);
-        }
+        String[] selected = getRand(numSelected,row,rowSize); //select numSelected number of random cells and store in array
 
-        System.out.println(all);
-        String[] selected = getRand(numSelected,all); //select numSelected number of random cells and store in array
     }
 
     /*
         @param num - number of cells to select
-        @param all - arraylist of all cells
+        @param all - row of all cells
+        @param rowSize - length of row to consider
 
-        @requires rowSize>0, num>=0, all!=null
-        @ensures returned array contains num number of randomly chosen, unique, cells from all
+        @requires num>=0, row.length=rowSize
+        @ensures returned array contains num number of randomly chosen, unique, cells from row
     */
-    private static String[] getRand(int num, ArrayList<String> all){
+    private static String[] getRand(int num, Row row, int rowSize){
+        var dataFormatter = new DataFormatter();
         int i=0;
         String[]selected = new String[num]; 
         HashSet<Integer>selIndex = new HashSet<>(); //chosen indecies, prevent picking the same one
         
         while(i<num){ 
-            int rand = (int)(Math.random()*all.size()); //pick a random index. if it hasnt been chosen, use it
+            int rand = (int)(Math.random()*rowSize); //pick a random index. if it hasnt been chosen, use it
             if(!selIndex.contains(rand)){ 
-                selected[i]=all.get(rand);
+                selected[i]=dataFormatter.formatCellValue(row.getCell(rand));
                 selIndex.add(rand);
                 i++;
             }
         }
-        
         return selected;
     }
 
