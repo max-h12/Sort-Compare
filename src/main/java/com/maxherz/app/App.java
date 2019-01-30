aaa7c7ded733371530b370c1fba028040989570b
Max Herz <maxopie@gmail.com>
2019-01-29 21:47:56 -0500

    Initial commit

---
+++ src/main/java/com/maxherz/app/App.java
@@ -0,0 +1,20 @@
+package com.maxherz.app;
+
+import org.apache.poi.openxml4j.exceptions.*;
+import org.apache.poi.ss.usermodel.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator; 
+
+/**
+ * Hello world!
+ *
+ */
+public class App {       
+    public static final String SAMPLE_XLSX_FILE_PATH = "./sample-xlsx-file.xlsx";
+
+    public static void main( String[] args )
+    {
+        System.out.print("Hello Worlds");
+    }
+}
