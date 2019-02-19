5e36655abfd6a9d40f576d9d66331ca10542a60d
Max Herz <maxopie@gmail.com>
2019-02-19 18:13:14 -0500

    BogoSort, Multiple Trials, Documentation, A bunch more test words.

--- src/main/java/com/maxherz/app/sortCompare.java
+++ src/main/java/com/maxherz/app/sortCompare.java
@@ -1,6 +1,7 @@
 package com.maxherz.app;
 
 import org.apache.poi.openxml4j.exceptions.*;
+import org.apache.poi.ss.formula.functions.Column;
 import org.apache.poi.ss.usermodel.*;
 import java.io.File;
 import java.io.IOException;
@@ -17,54 +18,57 @@ public class sortCompare {
         Workbook workbook = WorkbookFactory.create(new File(SAMPLE_XLSX_FILE_PATH)); // get workbook
         Sheet sheet = workbook.getSheetAt(0); // first sheet
         Row row = sheet.getRow(0); // row
-        final int rowSize = 11; // rowsize. saves space
-        int numSelected = 4; // how many random numbers to sample
+        
+        final int rowSize = 4408; // rowsize. saves space
+        final int numSelected = 50; // how many random numbers to sample
+        final int trials = 5; //how many times to randomly sample and time
 
-        String[] selected = getRand(numSelected, row, rowSize); // select numSelected number of random cells and store
+        double avgSel = 0;
+        double avgIns = 0;
+        double avgMerge = 0;
+        double avgQuick = 0;
+        double avgBogo=0;
 
-        System.out.println("Selected:"); // show chosen numbers
-        printArray(selected);
-        System.out.println();
+        for(int i=0;i<trials;i++){
+            String[] selected = getRand(numSelected, row, rowSize); // select numSelected number of random cells and store
 
-        String[] selected2 = selected.clone(); // copy unsorted list so we can retime
-        double selSortTime = selSort(selected); // selection sort
-        System.out.println("Selection Sorted:"); // array after selection sort
-        printArray(selected);
-        System.out.println();
-        System.out.println("Time for selection sort: " + selSortTime); // time for selection sort
+            String[] selected2 = selected.clone(); // copy unsorted list so we can retime
+            double selSortTime = selSort(selected); // selection sort
+            avgSel += (selSortTime)/trials;//add fraction to total
 
-        selected = selected2.clone(); // copy unsorted list so we can retime
-        double inSortTime = inSort(selected); // selection sort
-        System.out.println("Insertion Sorted:"); // array after selection sort
-        printArray(selected);
-        System.out.println();
-        System.out.println("Time for Insertion sort: " + inSortTime); // time for selection sort
+            selected = selected2.clone(); 
+            double inSortTime = inSort(selected); 
+            avgIns += (inSortTime)/trials;
 
-        selected = selected2.clone(); // copy back unsorted array to resort
-        double mergeSortTime = mergeMaster(selected); // time merge sort
-        System.out.println("Merge Sorted:"); // array after merge sort
-        printArray(selected);
-        System.out.println();
-        System.out.println("Time for merge sort: " + mergeSortTime); // time for merge sort
+            selected = selected2.clone(); 
+            double mergeSortTime = mergeMaster(selected); 
+            avgMerge += (mergeSortTime)/trials;
+            
+            selected = selected2.clone(); 
+            double quickSortTime = quickSortMaster(selected);
+            avgQuick += (quickSortTime)/trials;
 
-        selected = selected2.clone(); // copy back unsorted array to resort
-        double quickSortTime = quickSortMaster(selected); // time merge sort
-        System.out.println("Quick Sorted:"); // array after merge sort
-        printArray(selected);
-        System.out.println();
-        System.out.println("Time for quick sort: " + quickSortTime); // time for merge sort
+            selected = selected2.clone(); 
+            double bogoSortTime = bogoSortMaster(selected); 
+            avgBogo += (bogoSortTime)/trials; 
+
+        }
+
+        System.out.println("Time in milliseconds for "+trials+" independent trials below.");
+        System.out.println("Average time for selection sort: " + Math.round(avgSel*100)/100 +" milliseconds"); 
+        System.out.println("Average time for Insertion sort: " + Math.round(avgIns*100)/100 +" milliseconds"); 
+        System.out.println("Average time for merge sort: " + Math.round(avgMerge*100)/100 +" milliseconds"); 
+        System.out.println("Average time for quick sort: " + Math.round(avgQuick*100)/100 +" milliseconds"); 
+        System.out.println("Average time for bogo sort: " + Math.round(avgBogo*100)/100 +" milliseconds");
+
 
     }
 
     /*
      * @param num - number of cells to select
-     * 
      * @param all - row of all cells
-     * 
      * @param rowSize - length of row to consider
-     * 
      * @requires num>=0, row.length=rowSize
-     * 
      * @ensures returned array contains num number of randomly chosen, unique, cells
      * from row
      */
@@ -73,7 +77,6 @@ public class sortCompare {
         int i = 0;
         String[] selected = new String[num];
         HashSet<Integer> selIndex = new HashSet<>(); // chosen indecies, prevent picking the same one
-
         while (i < num) {
             int rand = (int) (Math.random() * rowSize); // pick a random index. if it hasnt been chosen, use it
             if (!selIndex.contains(rand)) {
@@ -87,7 +90,6 @@ public class sortCompare {
 
     /*
      * @param arr - array to time
-     * 
      * @returns time in milliseconds to sort arr
      */
     private static double selSort(String arr[]) {
@@ -108,12 +110,11 @@ public class sortCompare {
             arr[j + 1] = key;
         }
         long endTime = System.nanoTime();
-        return ((endTime - startTime) / 1000000);
+        return ((endTime - startTime)/1000000);
     }
 
     /*
      * @param arr - array to time
-     * 
      * @returns time in milliseconds to sort arr
      */
     private static double inSort(String arr[]) {
@@ -125,11 +126,7 @@ public class sortCompare {
         for (i = 1; i < n; i++) {
             key = arr[i];
             j = i - 1;
-
-            /*
-             * Move elements of arr[0..i-1], that are greater than key, to one position
-             * ahead of their current position
-             */
+            
             while (j >= 0 && arr[j].compareTo(key)>0) {
                 arr[j + 1] = arr[j];
                 j = j - 1;
@@ -142,7 +139,6 @@ public class sortCompare {
 
     /*
      * @param arr - array to time
-     * 
      * @returns time in milliseconds to sort arr
      */
     private static double mergeMaster(String[] arr) {
@@ -154,9 +150,7 @@ public class sortCompare {
 
     /*
      * @param arr - array to sort
-     * 
      * @param n - length to sort through
-     * 
      * @ensures arr will be sorted according to String preorder
      */
     private static void mergeSort(String[] arr, int n) {
@@ -181,15 +175,10 @@ public class sortCompare {
 
     /*
      * @param arr - array to store values
-     * 
      * @param l - left array to merge
-     * 
      * @param r - right array to merge
-     * 
      * @param left - left index
-     * 
      * @param right - right index
-     * 
      * @ensures values of l and r will be merged according to String preorder
      */
     private static void merge(String[] arr, String[] l, String[] r, int left, int right) {
@@ -212,7 +201,6 @@ public class sortCompare {
 
     /*
      * @param arr - array to print
-     *
      * @ensures arr is printed to console
      */
     private static void printArray(String[] arr) {
@@ -221,14 +209,19 @@ public class sortCompare {
         }
     }
 
+      /*
+     * @param arr - array to partition
+     * @param low - low index to partion
+     * @param high - pivot item
+     * @ensures arr is printed to console
+     */
     private static int partition(String arr[], int low, int high) 
     { 
         String pivot = arr[high];  
         int i = (low-1); // index of smaller element 
         for (int j=low; j<high; j++) 
         { 
-            // If current element is smaller than or 
-            // equal to pivot 
+
             if (arr[j].compareTo(pivot)<=0) 
             { 
                 i++; 
@@ -245,6 +238,12 @@ public class sortCompare {
         return i+1; 
     } 
   
+     /*
+     * @param arr - array to print
+     * @param low - starting index of sort
+     * @param high - ending index to sort
+     * @ensures arr is sorted 
+     */
     private static void quickSort(String arr[], int low, int high) 
     { 
         if (low < high) 
@@ -255,10 +254,63 @@ public class sortCompare {
         } 
     } 
 
+    /*
+     * @param arr - array to sort
+     * @returns time in milliseconds to sort arr
+     */
     private static double quickSortMaster(String arr[]){
         long startTime = System.nanoTime();
         quickSort(arr, 0, arr.length-1);
         long endTime = System.nanoTime();
         return ((endTime - startTime) / 1000000);
     }
+
+     /*
+     * @param arr - array to sort
+     * @returns time in milliseconds to sort arr
+     */
+    private static double bogoSortMaster(String arr[]){
+        long startTime = System.nanoTime();
+        while(!isSorted(arr)){
+            shuffle(arr);
+        }
+        long endTime = System.nanoTime();
+        return ((endTime - startTime) / 1000000);
+    }
+
+     /*
+     * @param arr - array to sort
+     * @ensures arr is randomly shuffeled around
+     */
+    private static void shuffle(String arr[]) 
+    { 
+         for (int i=1; i <= arr.length-1; i++) 
+             swap(arr, i, (int)(Math.random()*i)); 
+    } 
+
+     /*
+     * @param arr - array to swap indecies
+     * @param i - first index to swap
+     * @param j - second index to swap
+     * @ensures #arr[i] = arr[j] and #arr[j]=arr[i]
+     */
+    private static void swap(String arr[], int i, int j) 
+    { 
+        String temp = arr[i]; 
+        arr[i] = arr[j]; 
+        arr[j] = temp; 
+    } 
+
+     /*
+     * @param arr - array to check
+     * @returns true if arr is sorted, false otherwise 
+     */
+    private static boolean isSorted(String arr[]) 
+    { 
+        for (int i=1; i<arr.length; i++) 
+            if (arr[i].compareTo(arr[i-1])<0) 
+                return false; 
+        return true; 
+    } 
+
 }
